080e3f37412811cb32f8393c27e01af56ab85bc4
devEducaua <schokito4@gmail.com>
2025-08-11 20:30:41 -0300

    feat: add seaborgium support

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
+let specialWord = false;
 
 document.getElementById("btn").addEventListener("click", () => {
     const p = document.getElementById("p");
     p.style.fontFamily = "monospace";
     p.style.fontStyle = "normal"
-    
-    let word = "";
 
     const vowels = ['a', 'e', 'u', 'i', 'o'];
+    let word = "";
 
+    
     const v1 = vowels[Math.floor(Math.random() * vowels.length)];
     const v2 = vowels[Math.floor(Math.random() * vowels.length)];
     const v3 = vowels[Math.floor(Math.random() * vowels.length)];
     const v4 = vowels[Math.floor(Math.random() * vowels.length)];
     const v5 = vowels[Math.floor(Math.random() * vowels.length)];
 
-    if (Math.floor((Math.random() * 1000) + 1) < 2) {
-        word = "molibdênio";
-    } else {
-        word = `m${v1}l${v2}b${v3}d${v4}n${v5}`;
-    }
+    const num = Math.floor(Math.random() * 1000) + 1;
 
-    if (v1 == v2 && v2 == v3 && v3 == v4 && v4 == v5 || word == "molibdênio") {
+    if (specialWord) {
+        if (num < 2) {
+            word = "seabórgio";
+        } else {
+            word = `s${v1}${v2}b${v3}rg${v4}${v5}`
+
+        }
+    } else {
+        if (num < 2) {
+            word = "molibdênio";
+        } else {
+            word = `m${v1}l${v2}b${v3}d${v4}n${v5}`;
+        }
+    }
+    
+    if (v1 == v2 && v2 == v3 && v3 == v4 && v4 == v5) {
         alert("You get a special word");
         p.style.fontFamily = "Times New Roman";
         p.style.fontStyle = "italic";
+        specialWord = true;
     }
 
     p.textContent = word;
